a1efcee5b7ceddd7d80a70a2137bede1b34f5f35
chenjun0516 <136362633@qq.com>
2017-02-07 13:37:29 +0800

    修改评价

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,9 +1,48 @@
 <template>
-	<div>I am ratings</div>
+	<div class="ratings">
+		<ul>
+			<li v-for="item in goods" class="menu-item">
+				{{ item.text }}
+				{{ item.text }}
+				{{ item.text }}
+				{{ item.text }}
+				{{ item.text }}
+				{{ item.text }}
+			</li>
+		</ul>
+	</div>
 </template>
-<script type="text/ecmascript-6">
-	export default {};
+<script type="text-ecmascript-6">
+	const ERR_OK = 0;
+	export default {
+		props: {
+			seller: {
+				type: Object
+			}
+		},
+		data () {
+			return {
+				goods: []
+			}
+		},
+		created () {
+			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+
+			this.$http.get('/api/ratings').then ((response) => {
+				response = response.body;
+				if (response.errno === ERR_OK) {
+					this.goods = response.data;
+				}
+			})
+		}
+	};
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-	
+	.ratings
+		position: absolute
+		top: 174px
+		bottom: 0
+		left: 0
+		width: 100%
+		overflow:auto
 </style>
