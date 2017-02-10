9113e96dcbf220041f11cdbc809604fd4b7789de
chenjun0516 <136362633@qq.com>
2017-02-10 14:56:11 +0800

    购物车组件

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -27,18 +27,22 @@
 								<div class="price">
 									<span class="now">￥{{ food.price }}</span><span class="old" v-show="food.oldPrice">￥{{ food.oldPrice }}</span>
 								</div>
+								<div class="cartcontrol-wrapper">
+									<cartcontrol :food="food"></cartcontrol>
+								</div>
 							</div>
 						</li>
 					</ul>
 				</li>
 			</ul>
 		</div>
-		<shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>	
+		<shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>	
 	</div>
 </template>
 <script type="text/ecmascript-6">
 	import BScroll from 'better-scroll';
 	import shopcart from 'components/shopcart/shopcart';
+	import cartcontrol from 'components/cartcontrol/cartcontrol';
 
 	const ERR_OK = 0;
 
@@ -65,6 +69,18 @@
 					}
 				}
 				return 0;
+			},
+			selectFoods () {
+				let foods = [];
+				this.goods.forEach((good) => {
+					good.foods.forEach((food) => {
+						if (food.count) {
+							foods.push(food);
+						}
+					});
+				});
+
+				return foods;
 			}
 		},
 		created () {
@@ -96,6 +112,7 @@
 				});
 
 				this.foodScroll = new BScroll(this.$els.foodsWrapper, {
+					click: true,
 					probeType: 3
 				});
 
@@ -115,7 +132,8 @@
 			}
 		},
 		components: {
-			shopcart
+			shopcart,
+			cartcontrol
 		}
 	};
 </script>
@@ -222,4 +240,8 @@
 							text-decoration: line-through
 							font-size: 10px
 							color: rgb(147,153,159)
+					.cartcontrol-wrapper
+						position: absolute
+						right: 0
+						bottom: 12px
 </style>
--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,15 +3,18 @@
 		<div class="content">
 			<div class="content-left">
 				<div class="logo-wrapper">
-					<div class="logo">
-						<i class="icon-shopping_cart"></i>
+					<div class="logo" :class="{'highlight':totalCount>0}">
+						<i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
 					</div>
+					<div class="num" v-show="totalCount>0">{{ totalCount }}</div>
 				</div>
-				<div class="price">￥0</div>
+				<div class="price" :class="{'highlight':totalPrice>0}">￥{{ totalPrice }}</div>
 				<div class="desc">另需配送费￥{{deliveryPrice}}元</div>
 			</div>
 			<div class="content-right">
-				<div class="pay">￥{{ minPrice }}起送</div>
+				<div class="pay" :class="payClass">
+					{{ payDesc }}
+				</div>
 			</div>
 		</div>
 	</div>
@@ -19,6 +22,12 @@
 <script type="text/ecmascript-6">
 	export default {
 		props: {
+			selectFoods: {
+				type: Array,
+				default () {
+					return [];
+				}
+			},
 			deliveryPrice: {
 				type: Number,
 				default: 0
@@ -26,6 +35,39 @@
 			minPrice: {
 				type: Number,
 				default: 0
+			}
+		},
+		computed: {
+			totalPrice () {
+				let total = 0;
+				this.selectFoods.forEach((food) => {
+					total += food.price * food.count;
+				});
+				return total;
+			},
+			totalCount () {
+				let count = 0;
+				this.selectFoods.forEach((food) => {
+					count += food.count;
+				});
+				return count;
+			},
+			payDesc () {
+				if (this.totalPrice === 0) {
+					return `￥${this.minPrice}元起送`;
+				} else if (this.totalPrice < this.minPrice) {
+					let diff = this.minPrice - this.totalPrice;
+					return `还差￥${diff}元起送`;
+				} else {
+					return '去结算';
+				}
+			},
+			payClass () {
+				if (this.totalPrice < this.minPrice) {
+					return 'not-enough';
+				} else {
+					return 'enough';
+				}
 			}
 		}
 	};
@@ -62,10 +104,28 @@
 						text-align: center
 						border-radius: 50%
 						background: #2b343c
+						&.highlight
+							background: rgb(0,160,220)		
 						.icon-shopping_cart
 							line-height: 44px
 							font-size: 24px
 							color: #80858a
+							&.highlight
+								color: #fff
+					.num
+						position: absolute
+						top: 0
+						right: 0
+						width: 24px
+						height: 16px
+						line-height: 16px
+						text-align: center
+						border-radius: 16px
+						font-size: 9px
+						font-weight: 700
+						color: #fff
+						background: rgb(240,20,20)
+						box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4)
 				.price
 					display: inline-block
 					vertical-align: top
@@ -77,6 +137,8 @@
 					font-size: 16px
 					font-weight: 700
 					color: rgba(255,255,255,0.4)
+					&.highlight
+						color: #fff
 				.desc
 					display: inline-block
 					vertical-align: top
@@ -95,5 +157,9 @@
 					color: rgba(255,255,255,0.4)
 					font-weight: 700
 					background: #2b333b
-					
+					&.not-enough
+						background: #2b333b
+					&.enough
+						background: #00b43c
+						color: #fff
 </style>
