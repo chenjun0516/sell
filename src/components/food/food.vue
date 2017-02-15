e153ce50bf995131e6caddd0fafbd083b754e254
chenjun0516 <136362633@qq.com>
2017-02-15 17:49:16 +0800

    consummate food && commit ratings

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -15,18 +15,49 @@
 				<div class="price">
 					<span class="now">￥{{ food.price }}</span><span class="old" v-show="food.oldPrice">￥{{ food.oldPrice }}</span>
 				</div>
+				<div class="cartcontrol-wrapper">
+					<cartcontrol :food="food"></cartcontrol>
+				</div>
+				<div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
 			</div>
-			<div class="cartcontrol-wrapper">
-				<cartcontrol :food="food"></cartcontrol>
+			<split v-show="food.info"></split>
+			<div class="info" v-show="food.info">
+				<h1 class="title">商品介绍</h1>
+				<p class="text">{{ food.info }}</p>
 			</div>
-			<div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">加入购物车</div>
+			<split></split>
+			<div class="rating">
+				<h1 class="title">商品评价</h1>
+				<ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+				<div class="rating-wrapper">
+					<ul v-show="food.ratings && food.ratings.length">
+						<li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
+							<div class="user">
+								<span class="name">{{ rating.username }}</span>
+								<img class="avatar" width="12" height="12" :src="rating.avatar" alt="">
+							</div>
+							<div class="time">{{ rating.rateTime | formatDate }}</div>
+							<p class="text">
+								<span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}		
+							</p>
+						</li>
+					</ul>
+					<div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+				</div>
+			</div>
+
 		</div>
 	</div>
 </template>
-<script type="text-ecmascript-6">
+<script type="text/ecmascript-6">
 	import BScroll from 'better-scroll';
 	import Vue from 'vue';
+	import {formatDate} from '../../common/js/date';
 	import cartcontrol from 'components/cartcontrol/cartcontrol';
+	import ratingselect from 'components/ratingselect/ratingselect';
+	import split from 'components/split/split';
+	
+	const ALL = 2;
 
 	export default {
 		props: {
@@ -36,12 +67,21 @@
 		},
 		data () {
 			return {
-				showFlag: false
+				showFlag: false,
+				selectType: ALL,
+				onlyContent: true,
+				desc: {
+					all: '全部',
+					positive: '推荐',
+					negative: '吐槽'
+				}
 			};
 		},
 		methods: {
 			show () {
 				this.showFlag = true;
+				this.selectType = ALL;
+				this.onlyContent = true;
 				this.$nextTick(() => {
 					if (!this.scroll) {
 						this.scroll = new BScroll(this.$els.food, {
@@ -55,20 +95,54 @@
 			hide () {
 				this.showFlag = false;
 			},
-			addFirst (evebt) {
+			addFirst (event) {
 				if (!event._constructed) {
 					return;
 				} else {
-					Vue.set(this.food, 'count', 1)
+					Vue.set(this.food, 'count', 1);
+				}
+			},
+			needShow (type, text) {
+				if (this.onlyContent && !text) {
+					return false;
+				};
+				if (this.selectType === ALL) {
+					return true;
+				} else {
+					return type === this.selectType;
 				}
 			}
 		},
+		events: {
+			'ratingtype.select' (type) {
+				this.selectType = type;
+				this.$nextTick(() => {
+					this.scroll.refresh();
+				});
+			},
+			'content.toggle' (onlyContent) {
+				this.onlyContent = onlyContent;
+				this.$nextTick(() => {
+					this.scroll.refresh();
+				});
+			}
+		},
+		filters: {
+			formatDate (time) {
+				let date = new Date(time);
+				return formatDate(date, 'yyyy-MM-dd hh:mm');
+			}
+		},
 		components: {
-			cartcontrol
+			cartcontrol,
+			ratingselect,
+			split
 		}
 	};
 </script>
 <style lang="stylus" rel=stylesheet/stylus>
+	@import "../../stylus/mixin.styl"
+
 	.food
 		position: fixed
 		left: 0
@@ -103,6 +177,7 @@
 					font-size: 20px
 					color: #fff
 		.content
+			position: relative
 			padding: 18px
 			.title
 				line-height: 14px
@@ -133,24 +208,84 @@
 					text-decoration: line-through
 					font-size: 10px
 					color: rgb(147,153,159)
-		.cartcontrol-wrapper
-			position: absolute
-			right: 12px
-			bottom: 12px
-		.buy
-			position: absolute
-			right: 18px
-			bottom: 18px
-			z-index: 10px
-			height: 24px
-			line-height: 24px
-			padding: 0 12px
-			text-align: center
-			box-sizing: border-box
-			font-size: 10px
-			border-radius: 12px
-			color: #fff
-			background: rgb(0,160,220)
+			.cartcontrol-wrapper
+				position: absolute
+				right: 12px
+				bottom: 12px
+			.buy
+				position: absolute
+				right: 18px
+				bottom: 18px
+				z-index: 10px
+				height: 24px
+				line-height: 24px
+				padding: 0 12px
+				text-align: center
+				box-sizing: border-box
+				
+				font-size: 10px
+				border-radius: 12px
+				color: #fff
+				background: rgb(0,160,220)
+		.info
+			padding: 18px
+			.title
+				line-height: 14px
+				font-size: 14px
+				color: rgb(7,17,27)
+			.text
+				margin: 6px 8px 0
+				line-height: 24px
+				font-size: 12px
+				font-weight: 200
+				color: rgb(77,85,93)
+		.rating
+			padding-top: 18px
+			.title
+				line-height: 14px
+				font-size: 14px
+				margin-left: 18px
+				color: rgb(7,17,27)
+			.rating-wrapper
+				padding: 0 18px
+				.rating-item
+					position: relative
+					padding: 16px 0
+					border-1px(rgba(7,17,27,0.1))
+					.user
+						position: absolute
+						top: 16px
+						right: 0
+						line-height: 12px
+						font-size: 0
+						.name
+							display: inline-block
+							vertical-align: top
+							margin-right: 6px
+							font-size: 10px
+							color: rgb(147,153,159)
+						.avatar
+							border-radius: 50%
+					.time
+						margin-bottom: 6px
+						line-height: 12px
+						font-size: 10px
+						color: rgb(147,153,159)
+					.text
+						line-height: 16px
+						font-size: 12px
+						color: rgb(7,17,27)
+						.icon-thumb_up,.icon-thumb_down
+							margin-right: 4px
+							font-size: 12px
+						.icon-thumb_up
+							color: rgb(0,160,220)
+						.icon-thumb_down
+							color: rgb(147,153,159)
+				.no-rating
+					padding: 16px 0
+					font-size: 12px
+					color: rgb(147,153,159)
 				
 			
 </style>
--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,18 +1,37 @@
 <template>
 	<div class="ratings">
-		<ul>
-			<li v-for="item in goods" class="menu-item">
-				{{ item.text }}
-				{{ item.text }}
-				{{ item.text }}
-				{{ item.text }}
-				{{ item.text }}
-				{{ item.text }}
-			</li>
-		</ul>
+		<div class="ratings-content">
+			<div class="overview">
+				<div class="overview-left">
+					<h1 class="score">{{ seller.score }}</h1>
+					<div class="title">综合评分</div>
+					<div class="rank">高于周边商家{{ seller.rankRate}}%</div>
+				</div>
+				<div class="overview-right">
+					<div class="score-wrapper">
+						<span class="title">服务态度</span>
+						<star :size="36" :score="seller.serviceScore"></star>
+						<span class="score">{{ seller.serviceScore }}</span>
+					</div>
+					<div class="score-wrapper">
+						<span class="title">商品评分</span>
+						<star :size="36" :score="seller.foodScore"></star>
+						<span class="score">{{ seller.foodScore }}</span>
+					</div>
+					<div class="delivery-wrapper">
+						<span class="title">送达时间</span>
+						<span class="delivery">{{ seller.deliveryTime }}分钟</span>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
+	<shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
 </template>
-<script type="text-ecmascript-6">
+<script type="text/ecmascript-6">
+	import shopcart from 'components/shopcart/shopcart';
+	import star from 'components/star/star';
+
 	const ERR_OK = 0;
 	export default {
 		props: {
@@ -23,17 +42,21 @@
 		data () {
 			return {
 				goods: []
-			}
+			};
 		},
 		created () {
 			this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
 
-			this.$http.get('/api/ratings').then ((response) => {
+			this.$http.get('/api/ratings').then((response) => {
 				response = response.body;
 				if (response.errno === ERR_OK) {
 					this.goods = response.data;
 				}
-			})
+			});
+		},
+		components: {
+			shopcart,
+			star
 		}
 	};
 </script>
@@ -41,8 +64,63 @@
 	.ratings
 		position: absolute
 		top: 174px
-		bottom: 0
+		bottom: 48px
 		left: 0
 		width: 100%
-		overflow:auto
+		overflow:hidden
+		.overview
+			display: flex
+			padding: 18px 0
+			.overview-left
+				flex: 0 0 137px
+				padding: 6px 0
+				width: 137px
+				border-right: 1px solid rgba(7,17,27,.1)
+				text-align: center
+				.score
+					margin-bottom: 6px
+					line-height: 28px
+					font-size: 24px
+					color: rgb(255,153,0)
+				.title
+					margin-bottom: 8px
+					line-height: 12px
+					font-size: 12px
+					color: rgb(7,17,27)
+				.rank
+					line-height: 10px
+					font-size: 10px
+					color: rgb(147,153,159)
+			.overview-right
+				flex: 1
+				padding-left: 24px
+				.score-wrapper
+					margin-bottom: 8px
+					font-size: 0
+					.title
+						display: inline-block
+						vertical-align: top
+						line-height: 18px
+						font-size: 12px
+						color: rgb(7,17,27)
+					.star
+						display: inline-block
+						margin: 0 12px
+						vertical-align: top
+					.score
+						display: inline-block
+						vertical-align: top
+						line-height: 18px
+						font-size: 12px
+						color: rgb(255,153,0)
+				.delivery-wrapper
+					line-height: 18px
+					font-size: 0
+					.title
+						margin-right: 12px
+						font-size: 12px
+						color: rgb(7,17,27)
+					.delivery
+						font-size: 12px
+						color: rgb(147,153,159)			
 </style>
